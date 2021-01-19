9540a6368cdb1e76aee3c86874596d3e3ca64ab3
Crowdbotics <team@crowdbotics.com>
2021-01-19 22:15:09 +0000

    Initial commit.

---
+++ alpacaorders_23938/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alpacaorders_23938 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alpacaorders_23938.settings')
+
+application = get_wsgi_application()
